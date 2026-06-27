e7a9e9ce17746496f4aba4c7c0e54ffff041f325
Abdul Zabbar <a4zabbar@gmail.com>
2026-06-27 12:20:59 +0600

    secret key printing removed

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,6 @@ env.read_env(BASE_DIR / ".env")
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
-print(f"SECRET_KEY: {SECRET_KEY}")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
